529acc2c84d1dade598f0d21659f7f6af965f400
FernandoAltamirano <fernandoelialtamirano@gmail.com>
2025-05-10 12:22:22 -0500

    fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { TimerProvider } from '@/pages/timer/context';
 import { QueryProvider } from './providers/query-provider';
 import AppRouter from './router';
-import Modal from '@/shared/ui/molecules/Modal/modal';
+import Modal from '@/shared/ui/molecules/Modal/Modal';
 
 export const App = () => {
   return (
