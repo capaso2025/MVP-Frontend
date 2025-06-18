c0d029c2691650973321b5535f80f020d7130d0b
FernandoAltamirano <fernandoelialtamirano@gmail.com>
2025-06-18 11:03:41 -0500

    fix: Remove unused role check and update sidebar items based on authentication status

--- src/shared/ui/layouts/main-layout/index.tsx
+++ src/shared/ui/layouts/main-layout/index.tsx
@@ -1,17 +1,17 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../../organisms/sidebar';
 import { TimerFloating } from '@/pages/timer/components/TimerFloating';
 import HeaderActions from '@/pages/sections/components/organisms/HeaderActions';
 import { useResize } from '@/shared/hooks/use-resize';
 import { useEffect } from 'react';
 import { useRenderStore } from '@/shared/store/render-store';
-import { DEFAULT_MENU_ITEMS } from './menu-items';
+import { DEFAULT_MENU_ITEMS, NO_LOGGED_DEFAULT_MENU_ITEMS } from './menu-items';
 import { useAuthStore } from '@/features/auth/auth-store';
-import { useDummyStore } from '@/shared/store/dummy-store';
+// import { useDummyStore } from '@/shared/store/dummy-store';
 
 function MenuLayout() {
   const { isMobile } = useResize();
-  const role = useDummyStore((state) => state.role);
+  // const role = useDummyStore((state) => state.role);
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
   const setOpenedSidebar = useRenderStore((state) => state.setOpenedSidebar);
   useEffect(() => {
@@ -20,12 +20,16 @@ function MenuLayout() {
     }
   }, [isMobile, setOpenedSidebar]);
 
-  if (!isAuthenticated || role === 'teacher')
-    return <Navigate to="/login" replace />;
+  // if (!isAuthenticated || role === 'teacher')
+  //   return <Navigate to="/login" replace />;
 
   return (
     <div className="">
-      <Sidebar items={DEFAULT_MENU_ITEMS} />
+      <Sidebar
+        items={
+          !isAuthenticated ? NO_LOGGED_DEFAULT_MENU_ITEMS : DEFAULT_MENU_ITEMS
+        }
+      />
       {isMobile ? <></> : <div className="w-[300px]" />}
       <div className={isMobile ? 'w-auto' : 'ml-[300px]'}>
         <main className="mx-auto w-full p-4 lg:w-[1050px]">
