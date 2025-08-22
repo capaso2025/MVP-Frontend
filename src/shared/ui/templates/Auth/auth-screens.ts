3a456dd01992fd7200fe2e16bd49316483d1c11a
Fernando <62410565+FernandoAltamirano@users.noreply.github.com>
2025-08-22 13:27:01 -0500

    Delete src/shared/ui/templates/Auth directory

--- src/shared/ui/templates/Auth/auth-screens.ts
+++
@@ -1,7 +0,0 @@
-export type AuthScreen = (typeof AUTH_SCREENS)[keyof typeof AUTH_SCREENS];
-
-export const AUTH_SCREENS = {
-  LOGIN: 'login',
-  SIGNUP: 'signup',
-  SELECT_SIGNUP_ROAD: 'select-signup-road',
-};
